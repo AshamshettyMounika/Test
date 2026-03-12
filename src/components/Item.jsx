bb1aad299ac38dd65310282da4c2b44ea88089a2
AshamshettyMounika <ashamshettymounika@gmail.com>
2026-03-12 01:47:46 -0500

    Frontend UI updates: yellow rating stars, 3-column product layout, custom cursor, and product hover animation

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,8 +5,13 @@ const Item = ({product}) => {
   return (
     <div className='group relative'>
       <Link to={`/products/${product?.id}`}>
-      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-800/50 lg:aspect-none grup-hover:opacity-75 lg:h-80 h-96 border border-gray-700/50'>
-        <img src={product?.image} alt={product?.name} className='h-full w-full object-cover object-center lg:h-full lg:w-full' />
+      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-800/50 lg:aspect-none relative group'>
+        <img
+          src={product?.image}
+          alt={product?.name}
+          className='h-full w-full object-cover object-center transition-transform duration-500 group-hover:scale-110'
+        />
+        <div className='absolute top-0 left-0 w-full h-full bg-gray-300 opacity-0 group-hover:opacity-40 transition-all duration-500'></div>
       </div>
       </Link>
       <div className='mt-4 flex justify-between'>
